3a9dc25af2618e166b68f183c1c958962ef3f55f
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-03-24 16:14:19 -0700

    finally setup and beginning lab - creating dev branch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,39 +2,41 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
 jest.mock('twilio', () => () => ({
-  messages: {
-    create: jest.fn(),
-  },
+	messages: {
+		create: jest.fn(),
+	},
 }));
 
 describe('03_separation-of-concerns-demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+	beforeEach(() => {
+		return setup(pool);
+	});
 
-  it('creates a new order in our database and sends a text message', () => {
-    return request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 })
-      .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
-        expect(res.body).toEqual({
-          id: '1',
-          quantity: 10,
-        });
-      });
-  });
+	let order;
+	beforeEach(async () => {
+		order = await Order.insert({ quantity: 5 });
+	});
 
-  it('ASYNC/AWAIT: creates a new order in our database and sends a text message', async () => {
-    const res = await request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 });
+	it('creates a new order in our database and sends a text message', async () => {
+		const res = await request(app)
+			.post('/api/v1/orders')
+			.send({ quantity: 10 });
 
-    expect(res.body).toEqual({
-      id: '1',
-      quantity: 10,
-    });
-  });
+		expect(res.body).toEqual({
+			id: '2',
+			quantity: 10,
+		});
+	});
+
+	// it('finds all orders in our database and returns them', async () => {
+	// 	const res = await request(app).get('/api/v1/orders');
+
+	// 	expect(res.body).toEqual({
+	// 		id: '1',
+	// 		quantity: 10,
+	// 	});
+	// });
 });
